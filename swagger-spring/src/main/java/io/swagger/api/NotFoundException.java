6114e27ea2e52b1a73b4d7ebccafd184c472dbba
xuweng <2028403305@qq.com>
2019-09-01 13:08:30 +0800

    添加swagger-spring模块

---
+++ swagger-spring/src/main/java/io/swagger/api/NotFoundException.java
@@ -0,0 +1,11 @@
+package io.swagger.api;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-09-01T04:32:21.210Z")
+
+public class NotFoundException extends ApiException {
+    private int code;
+    public NotFoundException (int code, String msg) {
+        super(code, msg);
+        this.code = code;
+    }
+}
